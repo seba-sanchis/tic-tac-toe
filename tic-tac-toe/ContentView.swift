4ce69c8965536de07942ae563c774741e6c36f22
seba-sanchis <seba.sanchis@gmail.com>
2023-11-15 13:02:05 -0300

    Win and draw conditions

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -39,10 +39,30 @@ struct ContentView: View {
                             moves[i] = Move(player: .human, boardIndex: i)
                             isGameboardDisabled = true
                             
+                            if checkWinCondition(for: .human, in: moves) {
+                                print("Human wins.")
+                                return
+                            }
+                            
+                            if checkForDraw(in: moves) {
+                                print("Draw.")
+                                return
+                            }
+                            
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                                 isGameboardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves) {
+                                    print("Computer wins.")
+                                    return
+                                }
+                                
+                                if checkForDraw(in: moves) {
+                                    print("Draw.")
+                                    return
+                                }
                             }
                         }
                     }
@@ -68,6 +88,21 @@ struct ContentView: View {
         }
         
         return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+        
+        return false
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap { $0 }.count == 9
     }
 }
 
