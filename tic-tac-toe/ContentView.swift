591b23b913dc07dea62f528fd1d11ce9e0452af1
seba-sanchis <seba.sanchis@gmail.com>
2023-11-15 11:05:03 -0300

    Game board UI

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 8) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.mint)
+                            
+                            Circle()
+                                .stroke(Color.white, lineWidth: 3)
+                                .frame(width: 96)
+                                
+                                
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .background(.mint)
         }
-        .padding()
     }
 }
 
