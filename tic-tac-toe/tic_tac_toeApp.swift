114e4e6ce97e9674925aa49e49213d0ec2d0cdc9
seba-sanchis <seba.sanchis@gmail.com>
2023-11-14 23:51:55 -0300

    Initial Commit

---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Sebastian Sanchis on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
